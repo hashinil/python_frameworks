79bf39ff3d881627413076b2044c1d0d20b06880
Hashini <hashini.liyanage@gmail.com>
2023-08-02 15:02:01 +0200

    Add main page

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Weather Forcast for the Next Days")
+place = st.text_input("Place: ")
+days = st.slider("Forcast Days", min_value=1, max_value=5, help="Select the number of days.")
+options = st.selectbox("Select data to view", ("Temperature", "Sky"))
+st.subheader(f"{options} for the next {days} days in {place}")
