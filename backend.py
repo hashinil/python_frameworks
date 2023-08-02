e0393d20d77857e342ff6b644a8ec1b15be9cb5d
Hashini <hashini.liyanage@gmail.com>
2023-08-02 21:17:24 +0200

    get data and display

---
+++ backend.py
@@ -0,0 +1,17 @@
+import requests
+
+API_KEY = "9e417e3d7d80fd9fb27a19b557c2df86"
+
+
+def get_data(city, n_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    no_items = 8 * n_days
+    filtered_data = filtered_data[:no_items]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(city="colombo", n_days=2))
--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
+# Add GUI options
 st.title("Weather Forcast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forcast Days", min_value=1, max_value=5, help="Select the number of days.")
 options = st.selectbox("Select data to view", ("Temperature", "Sky"))
-st.subheader(f"{options} for the next {days} days in {place}")
 
 
-def get_data(days):
-    dates = ["2023-01-08", "2023-02-08", "2023-03-08"]
-    temperature = [10, 15, 12]
-    temperature = [temp * days for temp in temperature]
-    return dates, temperature
+if place:
+    try:
+        st.subheader(f"{options} for the next {days} days in {place}")
+        # Get data
+        filtered_data = get_data(place, days)
 
+        # Create temp plot
+        if options == "Temperature":
+            temperature = [data_row["main"]["temp"] / 10 for data_row in filtered_data]
+            dates = [data_row["dt_txt"] for data_row in filtered_data]
+            figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-d_val, t_val = get_data(days)
-figure = px.line(x=d_val, y=t_val, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        # Create image
+        if options == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_condition = [data_row["weather"][0]["main"] for data_row in filtered_data]
+            image_paths = [images[condition] for condition in sky_condition]
+            st.image(image_paths, width=100)
+    except KeyError:
+        st.write('Entered place is not exist')
